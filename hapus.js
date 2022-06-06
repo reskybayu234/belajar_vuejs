d20722e72133cb60026c976a9a91f0b331580506
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-06-06 15:57:27 +0700

    belajar vueJS

---
+++ hapus.js
@@ -0,0 +1,9 @@
+const object = [
+  { done: true, task: "Belajar VueJS di Petani Kode" },
+  { done: false, task: "Siram tanaman di kebun" },
+  { done: false, task: "Beli bibit kopi" },
+];
+
+for (const item in object) {
+  console.log();
+}
