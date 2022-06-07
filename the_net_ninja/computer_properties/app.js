3baa7828261719d19d1ad9f164f4264a83c6f431
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-06-07 17:07:34 +0700

    belajar vueJS

---
+++ the_net_ninja/computer_properties/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      judul: "Hallo Vue :)",
+      title: "Belajar VueJS",
+      author: "Resky Bayu Andhika",
+      showOrNot: true,
+      books: [
+        { title: "naruto", author: "masashi", isFav: true },
+        { title: "one piece", author: "oda", isFav: true },
+        { title: "Conan", author: "aoyama", isFav: false },
+      ],
+    };
+  },
+  methods: {
+    tombolShowOrNot() {
+      this.showOrNot = !this.showOrNot;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+      console.log(book.isFav);
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
+    },
+  },
+});
+
+app.mount("#app");
